88cb3fa3add0088bc3bb6608d601dad748a4acd0
parkuskus <ksmaachmad@gmail.com>
2024-09-12 17:43:38 +0700

    finalizing Perpangkatan.py

--- Modul 2/Perpangkatan.py
+++ Modul 2/Perpangkatan.py
@@ -1,5 +1,16 @@
 n = int(input("Masukkan nilai N: "))
 k = int(input("Masukkan nilai k: "))
 
-while n>0 :
-    
+n_awal = n 
+
+status = True
+while n>0 and status==True :
+    if n%2 == 0 :
+        n = n/2
+    else :
+        status = False
+
+if status== True :
+    print(f"{n_awal} merupakan perpangkatan {k}.")
+else :
+    print(f"{n_awal} bukan merupakan perpangkatan {k}.")
